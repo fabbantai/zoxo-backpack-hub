fd4f2e386c141b3583d13c67386c4e82ca801ed1
fabbantai <kurtivouge@gmail.com>
2026-03-20 20:34:51 +0530

    fixed all import issues

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,5 +1,5 @@
 import { useParams } from "react-router-dom";
-import products from "@/data/products";
+import { products } from "@/data/products";
 
 export default function CityPage() {
   const { city } = useParams();
